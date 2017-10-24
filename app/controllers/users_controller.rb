999a82731b882aa6ea04c919b4363bcfd820010c
haozheng <zhenghao22@gmail.com>
2017-10-24 17:51:36 -0400

    validation email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,34 @@
 class UsersController < ApplicationController
+  before_action :set_page, only: [:show,:edit,:update,:destroy]
+
+  def index
+
+  end
+
+
+  def show
+    set_page
+  end
+
+
   def new
+
   end
 
   def create
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
-      redirect_to '/users'
+      redirect_to user_path(user)
     else
       redirect_to '/users/new'
     end
+  end
+
+  private
+
+  def set_page
+    @user= User.find(params[:id])
   end
   def user_params
     params.require(:user).permit(:name, :email, :password)
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
 
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, length: {maximum: 225},
+      format:{ with: VALID_EMAIL_REGEX }
+
+
+
 end
