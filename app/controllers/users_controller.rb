48745fb018120f3f2ec0174dff2c5d60723a21d0
haozheng <zhenghao22@gmail.com>
2017-10-24 16:24:54 -0400

    password with bcrypt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,6 @@ class UsersController < ApplicationController
     end
   end
   def user_params
-    params.require(:user).permit(:name, :email, :password_digest)
+    params.require(:user).permit(:name, :email, :password)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
 
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
